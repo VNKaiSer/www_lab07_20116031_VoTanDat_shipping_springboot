66b08f5eaa28f68d6a3afe3a1b15f7b3ff7df5b7
VNKaiser <tandatvok16@gmail.com>
2023-11-22 17:25:28 +0700

    init

---
+++ src/main/java/com/example/lab07_shop_online_springboot/Lab07ShopOnlineSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab07_shop_online_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab07ShopOnlineSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab07ShopOnlineSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab07_shop_online_springboot/Lab07ShopOnlineSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab07_shop_online_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab07ShopOnlineSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
